120f368b978dbf8a25e08c7cdb78f2cde45a85f3
SHALAWA <bambukvova3@gmail.com>
2026-06-22 19:34:12 +0300

    Add files via upload

---
+++ Button(C#)/main.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+class program {
+    private static void button_callback(object sender, EventArgs e) {
+        exit(0);
+    }
+
+    private static void Main() {
+        Application.EnableVisualStyles();
+        Application.SetCompatibleTextRenderingDefault(false);
+
+        Label txt1 = new Label();
+        txt1.Text = "Welcome!";
+        txt1.Location = new Point(222, 145);
+
+        this.Controls.Add(txt1);
+
+        Button btn1 = new Button();
+        btn1.Location = new Point(223, 146);
+        btn1.Text = "Click";
+        btn1.Click += new EventHandler(this.button_callback);
+
+        this.Controls.Add(btn1);
+
+        Application.run();
+        
+    }
+}
